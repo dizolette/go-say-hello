91509d6c0db077f2af63d668d6b57d4ce0b40a8e
dizolette <albertusdwie@gmail.com>
2022-04-13 10:04:16 +0700

    update comment

--- sayHello.go
+++ sayHello.go
@@ -1,6 +1,14 @@
 package go_say_hello
 
 //upper case supaya bisa public access
+// setelah di push, harus buat tag.
+
+/*
+
+cara buat tag :
+git tag <nama-tag>
+git push origin <nama-tag>
+*/
 func SayHello() string {
 	return "hello"
 }
